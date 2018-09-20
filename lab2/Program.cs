2671ad1d944aec447f26560315772af714f90bd2
Mauricio Reyna <mauriciojreyna@gmail.com>
2018-09-20 12:06:19 -0400

    lab2

--- lab2/Program.cs
+++ lab2/Program.cs
@@ -10,7 +10,43 @@ namespace lab2
     {
         static void Main(string[] args)
         {
+            {
+                DateTime result, result1;
+                Console.WriteLine("Enter a valid datetime");
+                string d1 = Console.ReadLine();
 
+                if (!System.DateTime.TryParse(d1, out result))
+                {
+                    Console.WriteLine("Please enter a valid date");
+                    Console.ReadLine();
+                }
+                
+                Console.WriteLine("Enter a valid datetime");
+                string d2 = Console.ReadLine();
+
+                if (!System.DateTime.TryParse(d2, out result1))
+                {
+                    Console.WriteLine("Please enter a valid date");
+                    Console.ReadLine();
+                }
+                string output;
+
+                
+
+                var difference = result - result1;
+
+                
+
+                output = "Difference in dates is " + difference.Days + " days, " + difference.Hours + " hours, " + difference.Minutes + " Minutes.";
+                Console.WriteLine(output);
+                Console.ReadLine();
+
+               
+
+
+
+            }
         }
     }
 }
+
